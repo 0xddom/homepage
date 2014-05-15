68b87a5ab9d553a6650f7f0f7f30c72ca76ed3bc
KuroAku <danieldominguez05@gmail.com>
2014-05-15 02:05:45 +0200

    osu rank development

--- js/scripts.js
+++ js/scripts.js
@@ -26,6 +26,19 @@ function removeMascot() {
     $('main').addClass("plain");
 }
 
+//Function to set the rank in osu
+function setOsuRank() {
+    //TODO Here put the range numbers obtained by the proxy server.
+    //Globals name:
+    //  MUNDIAL_RANK
+    //  NATIONAL_RANK
+    //They are a number.
+    var rank_str = "Rango #" + MUNDIAL_RANK
+    var nrank_str = "Rango nacional #" + NATIONAL_RANK
+    $("a#rank").text(rank_str);
+    $("a#nrank").text(nrank_str);
+}
+
 $(document).ready(function(event) {
     var mascotEnable    = true;
     var mascotPath      = "images/mascots/"
@@ -38,4 +51,5 @@ $(document).ready(function(event) {
         setMascot(mascot);
         controlMascot(mascot, mascotMinWidth);
     } else { removeMascot(); }
+    setOsuRank();
 });
